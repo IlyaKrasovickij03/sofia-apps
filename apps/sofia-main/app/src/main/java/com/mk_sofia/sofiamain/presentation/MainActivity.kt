d65f4bc4fda199722ec86e43bc4e0267142f011e
Илья Красовицкий <krasovitskii.i@ati.su>
2024-06-23 13:24:26 +0300

    Перенес файл MainActivity в presentation

--- apps/sofia-main/app/src/main/java/com/mk_sofia/sofiamain/presentation/MainActivity.kt
+++ apps/sofia-main/app/src/main/java/com/mk_sofia/sofiamain/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mk_sofia.sofiamain
+package com.mk_sofia.sofiamain.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.mk_sofia.sofiamain.ui.theme.SofiaMainTheme
+import com.mk_sofia.sofiamain.core.ui.theme.SofiaMainTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
