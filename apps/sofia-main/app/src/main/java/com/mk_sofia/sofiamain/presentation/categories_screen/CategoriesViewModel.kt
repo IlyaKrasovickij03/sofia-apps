255bf5a578cd38f991661f05b6684450256c2ab5
Илья Красовицкий <krasovitskii.i@ati.su>
2024-06-24 16:55:46 +0300

    Реализовал event GetAllCategories и доабвил фабрику для ViewModel

--- apps/sofia-main/app/src/main/java/com/mk_sofia/sofiamain/presentation/categories_screen/CategoriesViewModel.kt
+++ apps/sofia-main/app/src/main/java/com/mk_sofia/sofiamain/presentation/categories_screen/CategoriesViewModel.kt
@@ -1,11 +1,18 @@
 package com.mk_sofia.sofiamain.presentation.categories_screen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.mk_sofia.sofiamain.domain.models.CategoryModel
+import com.mk_sofia.sofiamain.domain.repository.SofiaRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class CategoriesViewModel : ViewModel(), CategoriesContract {
+class CategoriesViewModel(
+    private val sofiaRepository: SofiaRepository,
+) : ViewModel(), CategoriesContract {
     private val _uiState = MutableStateFlow(CategoriesContract.State.initial())
     private val _effect = MutableStateFlow<CategoriesContract.Effect?>(null)
 
@@ -13,10 +20,34 @@ class CategoriesViewModel : ViewModel(), CategoriesContract {
     override val effect = _effect.asStateFlow()
 
     override fun event(event: CategoriesContract.Event) {
-        TODO("Not yet implemented")
+        when(event) {
+            CategoriesContract.Event.GetAllCategories -> getAllCategories()
+        }
     }
 
     override fun consume() {
         _effect.update { null }
+    }
+
+    private fun getAllCategories() {
+        viewModelScope.launch {
+            val categories = sofiaRepository.getAllCategories()
+            _uiState.update { currentState ->
+                currentState.copy(
+                    categories = categories
+                )
+
+            }
+        }
+    }
+
+    internal class Factory(
+        private val sofiaRepository: SofiaRepository,
+    ) : ViewModelProvider.NewInstanceFactory() {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T =
+            CategoriesViewModel(
+                sofiaRepository = sofiaRepository
+            ) as T
     }
 }
