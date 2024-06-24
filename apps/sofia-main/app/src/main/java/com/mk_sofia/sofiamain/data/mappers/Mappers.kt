2dcc9f85dde4469caaaf9c7fd321c183db08ad23
Илья Красовицкий <krasovitskii.i@ati.su>
2024-06-24 16:46:41 +0300

    Написал маппер данных из сетевого типа в доменный тип

---
+++ apps/sofia-main/app/src/main/java/com/mk_sofia/sofiamain/data/mappers/Mappers.kt
@@ -0,0 +1,16 @@
+package com.mk_sofia.sofiamain.data.mappers
+
+import com.mk_sofia.sofiamain.data.network.models.NetworkCategoryModel
+import com.mk_sofia.sofiamain.domain.models.CategoryModel
+
+fun List<NetworkCategoryModel>.toDomain(): List<CategoryModel> {
+    val listCategoryModel = mutableListOf<CategoryModel>()
+    this.forEach {
+        listCategoryModel.add(CategoryModel(
+            description = it.description,
+            id = it.id,
+            name = it.name
+        ))
+    }
+    return listCategoryModel
+}
